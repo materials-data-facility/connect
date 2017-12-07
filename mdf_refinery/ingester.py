76d10667b85ec2f4937ca4940d5eabad366e0baf
jgaff <jgaff@uchicago.edu>
2017-12-07 16:33:09 -0600

    Ingester streaming prototype
    Change ingester to accept wider range of input and stream feedstock more easily into Search. No longer requires files; can accept any iterable of iterables of feedstock dicts.

--- mdf_refinery/ingester.py
+++ mdf_refinery/ingester.py
@@ -10,16 +10,32 @@ from mdf_toolbox import toolbox
 NUM_SUBMITTERS = 5
 
 
-def ingest(ingest_client, feedstocks, batch_size=100):
+def ingest(ingest_client, feedstock, batch_size=100):
     """Ingests feedstock from file.
 
     Arguments:
     ingest_client (SearchClient): An authenticated client (see mdf_forge.toolbox)
-    feedstocks (str or list of str): The path(s) to feedstock to ingest.
+    feedstocks: (str or list of str or list of list of dict):
+                    Path(s) to files containing feedstock,
+                    or a list of lists of feedstock.
     batch_size (int): Max size of a single ingest operation. -1 for unlimited. Default 100.
     """
-    if type(feedstocks) is str:
-        feedstocks = [feedstocks]
+    # Fix single case of path not in list
+    if isinstance(feedstock, str):
+        feedstock = [feedstock]
+    # Feedstock must be iterable of str, or iterable of iterable of dict
+    if not hasattr(feedstock, "__iter__"):
+        raise TypeError("feedstock must be an iterable")
+    # If list of str, open paths
+    if isinstance(feedstock[0], str):
+        iter_stock = []
+        for path in feedstock:
+            iter_stock.append(open_feedstock(path))
+    # Otherwise, ensure is list of iterables
+    elif hasattr(feedstock[0], "__iter__"):
+        iter_stock = feedstock
+    else:
+        raise TypeError("feedstock must be an iterable of iterables")
 
     # Set up multiprocessing
     ingest_queue = multiprocessing.JoinableQueue()
@@ -27,7 +43,7 @@ def ingest(ingest_client, feedstocks, batch_size=100):
 
     # One reader
     reader = multiprocessing.Process(target=queue_ingests,
-                                     args=(ingest_queue, feedstocks, batch_size))
+                                     args=(ingest_queue, iter_stock, batch_size))
     # As many submitters as is feasible
     submitters = [multiprocessing.Process(target=process_ingests,
                                           args=(ingest_queue, ingest_client, killswitch))
@@ -46,15 +62,14 @@ def ingest(ingest_client, feedstocks, batch_size=100):
 def queue_ingests(ingest_queue, feedstocks, batch_size):
     for stock in feedstocks:
         list_ingestables = []
-        with open(stock, 'r') as feedstock:
-            for json_record in feedstock:
-                record = toolbox.format_gmeta(json.loads(json_record))
-                list_ingestables.append(record)
+        for entry in stock:
+            record = toolbox.format_gmeta(entry)
+            list_ingestables.append(record)
 
-                if batch_size > 0 and len(list_ingestables) >= batch_size:
-                    full_ingest = toolbox.format_gmeta(list_ingestables)
-                    ingest_queue.put(json.dumps(full_ingest))
-                    list_ingestables.clear()
+            if batch_size > 0 and len(list_ingestables) >= batch_size:
+                full_ingest = toolbox.format_gmeta(list_ingestables)
+                ingest_queue.put(json.dumps(full_ingest))
+                list_ingestables.clear()
 
         # Check for partial batch to ingest
         if list_ingestables:
@@ -79,3 +94,9 @@ def process_ingests(ingest_queue, ingest_client, killswitch):
             print("\nA Globus API Error has occurred. Details:\n", e.raw_json, "\n")
             continue
         ingest_queue.task_done()
+
+
+def open_feedstock(path):
+    with open(path, "r") as feedstock_file:
+        for json_entry in feedstock_file:
+            yield json.loads(json_entry)
